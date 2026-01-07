ce001374c96cab039e2bb264b80e1fb272cae1d2
Leialoha <github@leialoha.dev>
2026-01-07 10:57:33 -0600

    build: adds deployment scripts using github actions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Colors
-RED='\033[0;31m'
-GRAY='\033[0;90m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color (reset)
+RED='\033[0;31m';
+GRAY='\033[0;90m';
+GREEN='\033[0;32m';
+NC='\033[0m'; # No Color (reset)
 
-PACKAGE_FILE="package.json"
+PACKAGE_FILE="package.json";
 
-BUILD_DIRECTORY="build"
-BUNDLED_FILE="bundle.js"
-SCRIPT_FILE="file_atlas.sh"
+BUILD_DIRECTORY="build";
+BUNDLED_FILE="bundle.js";
+SCRIPT_FILE="file_atlas.sh";
 
 
 check_file() {
@@ -46,10 +46,14 @@ check_file $PACKAGE_FILE "Make sure this script is being ran from the base direc
 npx webpack
 
 # Check if the build was successful
-check_directory $BUILD_DIRECTORY "There could have been an issue with building with webpack." 
+check_directory $BUILD_DIRECTORY "There could have been an issue with building with webpack.";
 check_file "${BUILD_DIRECTORY}/${BUNDLED_FILE}" "Make sure this script is being ran from the base directory.";
 
 # Let's create a runnable script
-echo "#!/usr/bin/env node" > $BUILD_DIRECTORY/$SCRIPT_FILE
-cat $BUILD_DIRECTORY/$BUNDLED_FILE >> $BUILD_DIRECTORY/$SCRIPT_FILE
+echo "#!/usr/bin/env node" > $BUILD_DIRECTORY/$SCRIPT_FILE;
+cat $BUILD_DIRECTORY/$BUNDLED_FILE >> $BUILD_DIRECTORY/$SCRIPT_FILE;
 chmod +x $BUILD_DIRECTORY/$SCRIPT_FILE;
+
+# Let's copy all needed the items into the build folder
+cp ./package.json ./.atlasignore ./build/;
+cp -a ./docs ./build/;
